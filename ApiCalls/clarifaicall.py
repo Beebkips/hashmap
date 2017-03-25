f73cb84fdc300a7a879874d419ef13d20ef4394f
Vikas Mohandoss <vm2446@columbia.edu>
2017-03-25 15:52:53 -0700

    apicalls mod

--- ApiCalls/clarifaicall.py
+++ ApiCalls/clarifaicall.py
@@ -10,11 +10,27 @@ from clarifai.rest import Image as ClImage
   
   # The client takes the `APP_ID` and `APP_SECRET` you created in your Clarifai
   # account. You can set these variables in your environment as:
-  
-CLARIFAI_APP_ID = 'Re11ornY9K5Hd06foa9uCknPoaEY9y0w0MpHcGTa'
-CLARIFAI_APP_SECRET = 'XMEAHfXNyfA8TM1x76F1jQKJWcl7zBLRdFJuIdSN'
-app = ClarifaiApp(app_id=CLARIFAI_APP_ID,app_secret=CLARIFAI_APP_SECRET)
-model = app.models.get('general-v1.3')
-res = app.tag_files(['/Users/vikasmohandoss/Documents/hashmap/images/IMG_0953.jpg'])
-print(res)
+def getConceptsinImage( imageinput ):
+	CLARIFAI_APP_ID = 'Re11ornY9K5Hd06foa9uCknPoaEY9y0w0MpHcGTa'
+	CLARIFAI_APP_SECRET = 'XMEAHfXNyfA8TM1x76F1jQKJWcl7zBLRdFJuIdSN'
+	app = ClarifaiApp(app_id=CLARIFAI_APP_ID,app_secret=CLARIFAI_APP_SECRET)
+	model = app.models.get('general-v1.3')
+	res = app.tag_files([imageinput])
+
+	probconcept = {}
+	for output in res['outputs']:
+		for data in output['data']['concepts']:
+			probconcept[data['name']] = data['value']
+	return probconcept
+#print(res['outputs'][0]['data']['concepts'][0]['name'])
+
+outputdic = {}
+imageinput = '/Users/vikasmohandoss/Documents/hashmap/images/IMG_0953.jpg'
+outputdic = getConceptsinImage(imageinput)
+concepts = outputdic.keys()
+probvals = outputdic.values()
+print(concepts)
+print(probvals)
+
+
 
--- ApiCalls/exifcall.py
+++ ApiCalls/exifcall.py
@@ -1,9 +1,13 @@
 import exifread
 # Open image file for reading (binary mode)
-f = open('randomexif2.jpg', 'rb')
 
-# Return Exif tags
-tags = exifread.process_file(f)
+def getExifinImage( imageinput ):
+	f = open(imageinput, 'rb')
+	tags = exifread.process_file(f)
+	return tags
+
+imageinput = '/Users/vikasmohandoss/Documents/hashmap/images/IMG_0953.jpg'
+tags = getExifinImage( imageinput )
 for tag in tags.keys():
     if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
         print "Key: %s, value %s" % (tag, tags[tag])
